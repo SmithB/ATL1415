9822ec0e6c2b8e56815702a416be3baae4c3c69c
Ben Smith <besmith@uw.edu>
2023-08-29 17:53:15 -0400

    scripts/setup_slurm_run.py: optionally choose number of jobs for each task based on the distance from the pole

--- scripts/setup_slurm_run.py
+++ scripts/setup_slurm_run.py
@@ -11,6 +11,9 @@ import ATL1415
 import argparse
 import os
 import stat
+import numpy as np
+import re
+
 def setup_directories(run_dir):
     # setup the directories
     if not os.path.isdir(run_dir):
@@ -33,12 +36,18 @@ def get_last_task(run_name):
         last_file_num=0;
     return last_file_num
 
-def add_files_to_queue(run_name=None, task_list_file=None, task_glob=None, shell=None, env=None):
+def add_files_to_queue(run_name=None, task_list_file=None, task_glob=None, shell=None, env=None, R_range=None):
     last_file_num=get_last_task(run_name)
+    xy0_re=re.compile('--xy0\s+(\S+)\s+(\S+)')
     if task_list_file is not None:
+        add_count=0
         with open(task_list_file,'r') as fh:
-            add_count=0
             for line in fh:
+                if R_range is not None:
+                    xy=np.array([*map(float, xy0_re.search(line).groups())])
+                    R2=np.sum(xy**2)
+                    if (R2 < R_range[0]**2)  | (R2 >= R_range[1]**2) : 
+                        continue
                 last_file_num=last_file_num+1;
                 this_file=os.path.join(run_name,'queue','task_%d' % last_file_num)
                 with open(this_file,'w') as out_fh:
@@ -50,17 +59,32 @@ def add_files_to_queue(run_name=None, task_list_file=None, task_glob=None, shell
                         out_fh.write("source activate %s\n" % env)
                     out_fh.write('%s\n'% line.rstrip());
                 os.chmod(this_file, os.stat(this_file).st_mode | stat.S_IEXEC)
-            print(f"added {add_count} files to the queue")
+
 
     if task_glob is not None:
         task_files=glob.glob(task_glob)
         for file in task_files:
+            if R_range is not None:
+                skip=False
+                with open(file,'r') as fh:
+                    for line in fh:
+                        m=xy0_re.search(line).groups()
+                        if m is None:
+                            continue
+                        xy=np.array([*map(float, m.groups())])
+                        R2=np.sum(xy**2)
+                        if (R2 < R_range[0]**2)  | (R2 >= R_range[1]) : 
+                            skip=True
+                if skip:
+                    continue
             last_file_num=last_file_num+1;
             this_file=os.path.join(run_name,'queue','task_%d' % last_file_num)
+            add_count += 1
             os.rename(file, this_file)
-
+    print(f"added {add_count} files to the queue")
     with open(os.path.join(run_name,'last_task'),'w+') as last_task_fh:
         last_task_fh.write('%d\n'% last_file_num)
+    return add_count
 
 def __main__():
     parser = argparse.ArgumentParser()
@@ -69,20 +93,40 @@ def __main__():
     parser.add_argument('--task_glob', '-g', type=str, help='glob to match jobs')
     parser.add_argument('--environment','-e', type=str, default='ATL1415', help="environment that each job will activate")
     parser.add_argument('--shell','-s', type=str, default=None, help="shell to specify for each job (may not be needed)")
-    parser.add_argument('--jobs_per_task','-j', type=int, default=1, help="number of jobs per node")
+    parser.add_argument('--jobs_per_task','-j', type=int, help="number of jobs per node")
     parser.add_argument('--time','-t', type=str, default='02:00:00', help="time limit per job (hh:mm:ss)")
     parser.add_argument('--css', action='store_true', help="if set, the run will use the constraint=cssro argument, needed for ATL11")
     args=parser.parse_args()
-    
-    first_task=get_last_task(args.run_name)+1
+
+    R_dict=None
+    R_vals=[0, 1.e7]
+    if args.jobs_per_task is None:
+        R_dict={1.e8:{'dir':'queue_3cpu', 'ncpu':3, 'xy':[], 'src_file':[]},
+        6.e5:{'dir':'queue_14cpu','ncpu':14, 'xy':[], 'src_file':[]},
+        3.0e5:{'dir':'queue_20cpu','ncpu':20, 'xy':[], 'src_file':[]},
+        2.e5:{'dir':'queue_28cpu','ncpu':28, 'xy':[], 'src_file':[]}}
+        R_vals=list(R_dict.keys())[::-1]
     setup_directories(args.run_name)
-    add_files_to_queue(run_name=args.run_name, task_list_file=args.queue_file, task_glob=args.task_glob, shell=args.shell, env=args.environment)
-    last_task=get_last_task(args.run_name)
-    ATL1415.make_slurm_file(os.path.join(args.run_name, 'slurm_script.sh'),
-                          subs={'JOB_NAME':args.run_name,
-                                'TIME':args.time,
-                                'NUM_TASKS':str(args.jobs_per_task),
-                                'JOB_NUMBERS':f'{first_task}-{last_task}'}, css=args.css)
+    for ii in range(len(R_vals)-1):
+        first_task=get_last_task(args.run_name)
+        if R_dict is not None:
+            R_range=[R_vals[ii], R_vals[ii+1]]
+            slurm_file='slurm_run_'+ R_dict[R_vals[ii]]['dir']+'.sh'
+            N_tasks=R_dict[R_vals[ii]]['ncpu']
+        else:
+            R_range=None
+            slurm_file='slurm_run.sh'
+            N_tasks=args.jobs_per_task
+        N_added = add_files_to_queue(run_name=args.run_name, task_list_file=args.queue_file, task_glob=args.task_glob, shell=args.shell, env=args.environment, R_range=R_range)
+        last_task=get_last_task(args.run_name)
+        if N_added <1:
+            continue
+        ATL1415.make_slurm_file(os.path.join(args.run_name, slurm_file),
+                                subs={'JOB_NAME':args.run_name,
+                                      'TIME':args.time,
+                                      'NUM_TASKS':str(N_tasks),
+                                      'JOB_NUMBERS':f'{first_task+1}-{last_task}'}, css=args.css)
+
 
 if __name__ == '__main__':
     __main__()
