3a2f906858d7788e5d1ebb1438405a79575ac64f
Ben Smith <besmith@uw.edu>
2025-10-23 13:10:24 -0400

    ATL1415/tile_names.py: script to parse tile coordinates from tile names

--- ATL1415/__init__.py
+++ ATL1415/__init__.py
@@ -7,3 +7,5 @@ from .make_slurm_file import *
 from .assign_firn_variable import *
 from .SMB_corr_from_grid import SMB_corr_from_grid
 from .read_ATL11 import *
+from  .tile_names import tile_centers_from_files
+from  .tile_names import tile_centers_from_scripts
