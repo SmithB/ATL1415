5df9def737eadbae42f4a1f565a6aa44de2cf1ef
Ben Smith <bsmith@apl.washington.edu>
2026-01-27 11:23:24 -0800

    ATL1415/scripts/make_field_size_report.py : add main() method

--- ATL1415/scripts/make_field_size_report.py
+++ ATL1415/scripts/make_field_size_report.py
@@ -8,39 +8,40 @@ import glob
 import numpy as np
 import json
 
-tile_root='/discover/nobackup/projects/icesat2/ATL14_processing/rel004/south/'
-region='AA'
-step='prelim'
+def main():
 
-if len(sys.argv)==3:
-    tile_root=sys.argv[1]
-    region=sys.argv[2]
-    step=sys.argv[3]
-    step_dir=os.path.join(tile_root, region, step)
-else:
-    step_dir=sys.argv[1]
+    if len(sys.argv)==3:
+        tile_root=sys.argv[1]
+        region=sys.argv[2]
+        step=sys.argv[3]
+        step_dir=os.path.join(tile_root, region, step)
+    else:
+        step_dir=sys.argv[1]
 
-#print(step_dir)
-#sys.exit()
+    #print(step_dir)
+    #sys.exit()
 
-dst_directory=os.path.join(step_dir,  'field_sizes')
-if not os.path.isdir(dst_directory):
-    os.mkdir(dst_directory)
+    dst_directory=os.path.join(step_dir,  'field_sizes')
+    if not os.path.isdir(dst_directory):
+        os.mkdir(dst_directory)
 
-files=glob.glob(os.path.join(step_dir,'E*.h5'))
-for count, thefile in enumerate(files):
-    if np.mod(count, 500)==0:
-        print(f'{count} out of {len(files)}')
-    report={'file':thefile}
-    try:
-        with h5py.File(thefile,'r') as h5f:
-            for field in ['dz/dz','dz/sigma_dz']:
-                try:
-                    report[field]=list(h5f[field].shape)
-                except Exception:
-                    report[field]=None
-    except Exception:
-        pass
-    report_file=os.path.join(dst_directory, os.path.basename(thefile).replace('.h5','_report.json'))
-    with open(report_file,'w') as fh:
-        json.dump(report, fh)
+    files=glob.glob(os.path.join(step_dir,'E*.h5'))
+    for count, thefile in enumerate(files):
+        if np.mod(count, 500)==0:
+            print(f'{count} out of {len(files)}')
+        report={'file':thefile}
+        try:
+            with h5py.File(thefile,'r') as h5f:
+                for field in ['dz/dz','dz/sigma_dz']:
+                    try:
+                        report[field]=list(h5f[field].shape)
+                    except Exception:
+                        report[field]=None
+        except Exception:
+            pass
+        report_file=os.path.join(dst_directory, os.path.basename(thefile).replace('.h5','_report.json'))
+        with open(report_file,'w') as fh:
+            json.dump(report, fh)
+
+if __name__=='__main__':
+    main()
