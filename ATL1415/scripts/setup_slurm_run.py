e8e7b9c4db92b4f85b9805cc9e8df5bbb1c6c3c5
Ben Smith <bsmith@apl.washington.edu>
2026-07-20 10:46:43 -0700

    split radius tiers from setup_slurm_run.py (now see setup_ATL1415_run.py):  ATL1415/scripts/setup_slurm_run.py  docs/howto_AA.sh pyproject.toml  ATL1415/resources/radius_ncpu_tiers.json ATL1415/scripts/setup_ATL1415_run.py

--- ATL1415/scripts/setup_slurm_run.py
+++ ATL1415/scripts/setup_slurm_run.py
@@ -11,8 +11,6 @@ import ATL1415
 import argparse
 import os
 import stat
-import numpy as np
-import re
 
 def setup_directories(run_dir):
     # setup the directories
@@ -37,10 +35,8 @@ def get_last_task(run_name):
         last_file_num=0
     return last_file_num
 
-def add_files_to_queue(run_name=None, task_list_file=None,  shell=None, env=None, R_range=None, max_jobs=None, lines_per_task=1, line_count=None):
+def add_files_to_queue(run_name=None, task_list_file=None,  shell=None, env=None, max_jobs=None, lines_per_task=1, line_count=None):
     last_file_num=get_last_task(run_name)
-    xy0_re=re.compile('--xy0\s+(\S+)\s+(\S+)')
-    xyfile_re=re.compile('/E(\S*)_N(\S*).h5')
 
     add_count=0
     with open(task_list_file,'r') as fh:
@@ -50,17 +46,6 @@ def add_files_to_queue(run_name=None, task_list_file=None,  shell=None, env=None
             # fast forward to first requested line
             if line_number <= line_count[0]:
                 continue
-            if R_range is not None:
-                    try:
-                        xy=np.array([*map(float, xy0_re.search(line).groups())])
-                    except Exception:
-                        try:
-                            xy=1000*np.array([*map(float, xyfile_re.search(line).groups())])
-                        except Exception:
-                            continue
-                    R2=np.sum(xy**2)
-                    if (R2 < R_range[0]**2)  | (R2 >= R_range[1]**2) :
-                        continue
 
             task_lines.append(line)
             this_count += 1
@@ -97,50 +82,33 @@ def main():
     parser.add_argument('--environment','-e', type=str, default='ATL1415', help="environment that each job will activate")
     parser.add_argument('--shell','-s', type=str, default=None, help="shell to specify for each job (may not be needed)")
     parser.add_argument('--lines_per_task', type=int, default=1, help="combine this number of lines from the input into each task")
-    parser.add_argument('--jobs_per_task','-j', type=int, help="number of jobs per node")
+    parser.add_argument('--jobs_per_task','-j', type=int, required=True, help="number of jobs per node")
     parser.add_argument('--time','-t', type=str, default='02:00:00', help="time limit per job (hh:mm:ss)")
     parser.add_argument('--css', action='store_true', help="if set, the run will use the constraint=cssro argument, needed for ATL11")
     parser.add_argument('--max_jobs', type=int, default=4000, help="maximum number of task files to add to the queue in one call")
     args=parser.parse_args()
 
-    R_dict=None
-    R_vals=[0, 1.e7]
-    if args.jobs_per_task is None:
-        # if the number of jobs per task is none, select a number of tasks based on the --xy0 argument in the line or the tile location
-        R_dict={1.e8:{'dir':'queue_7cpu', 'ncpu':7, 'xy':[], 'src_file':[]},
-        5.0e5:{'dir':'queue_7cpu','ncpu':7, 'xy':[], 'src_file':[]},
-        4.0e5:{'dir':'queue_12cpu','ncpu':12, 'xy':[], 'src_file':[]},
-        3.0e5:{'dir':'queue_18cpu','ncpu':18, 'xy':[], 'src_file':[]},
-        2.e5:{'dir':'queue_28cpu','ncpu':28, 'xy':[], 'src_file':[]}}
-        R_vals=list(R_dict.keys())[::-1]
     setup_directories(args.run_name)
-    for ii in range(len(R_vals)-1):
-        if R_dict is not None:
-            R_range=[R_vals[ii], R_vals[ii+1]]
-            slurm_file='slurm_run_'+ R_dict[R_vals[ii]]['dir']+'.sh'
-            N_tasks=R_dict[R_vals[ii]]['ncpu']
-        else:
-            R_range=None
-            slurm_file='slurm_run.sh'
-            N_tasks=args.jobs_per_task
-        N_added=1
-        part_count=0
-        line_count=[-1]
-        while N_added > 0:
-            first_task=get_last_task(args.run_name)
-            N_added = add_files_to_queue(run_name=args.run_name, task_list_file=args.queue_file, shell=args.shell, env=args.environment, R_range=R_range, max_jobs=args.max_jobs, lines_per_task=args.lines_per_task, line_count=line_count)
-            if N_added <1:
-                continue
-            last_task=get_last_task(args.run_name)
-            this_slurm_file=slurm_file
-            if part_count > 0:
-                this_slurm_file=slurm_file.replace('.sh',f'_part{part_count}.sh')
-            ATL1415.make_slurm_file(os.path.join(args.run_name, this_slurm_file),
-                                subs={'JOB_NAME':args.run_name,
-                                      'TIME':args.time,
-                                      'NUM_TASKS':str(N_tasks),
-                                      'JOB_NUMBERS':f'{first_task+1}-{last_task}'}, css=args.css)
-            part_count += 1
+    slurm_file='slurm_run.sh'
+    N_tasks=args.jobs_per_task
+    N_added=1
+    part_count=0
+    line_count=[-1]
+    while N_added > 0:
+        first_task=get_last_task(args.run_name)
+        N_added = add_files_to_queue(run_name=args.run_name, task_list_file=args.queue_file, shell=args.shell, env=args.environment, max_jobs=args.max_jobs, lines_per_task=args.lines_per_task, line_count=line_count)
+        if N_added <1:
+            continue
+        last_task=get_last_task(args.run_name)
+        this_slurm_file=slurm_file
+        if part_count > 0:
+            this_slurm_file=slurm_file.replace('.sh',f'_part{part_count}.sh')
+        ATL1415.make_slurm_file(os.path.join(args.run_name, this_slurm_file),
+                            subs={'JOB_NAME':args.run_name,
+                                  'TIME':args.time,
+                                  'NUM_TASKS':str(N_tasks),
+                                  'JOB_NUMBERS':f'{first_task+1}-{last_task}'}, css=args.css)
+        part_count += 1
 
 if __name__ == '__main__':
     main()
--- docs/howto_AA.sh
+++ docs/howto_AA.sh
@@ -7,22 +7,22 @@ setup_ATL1415_region.py default_args/discover.txt default_args/latest_release.tx
 make_ATL1415_queue.py prelim /discover/nobackup/projects/icesat2/ATL14_processing/rel005/south/AA/input_args_AA.txt --min_xy 360000 
 
 # make the slurm run for AA_north
-setup_slurm_run.py --run_name AA_prelim_north -q 1415_queue_AA_prelim.txt --time 04:00:00  -e ATL14
+setup_ATL1415_run.py --run_name AA_prelim_north -q 1415_queue_AA_prelim.txt --time 04:00:00  -e ATL14
 
 # make the field size reports:
 scripts/make_field_size_report.py /discover/nobackup/projects/icesat2/ATL14_processing/rel005/south/AA/prelim
 
 # make the AA_matched queue for north:
 scripts/make_ATL1415_queue.py matched /discover/nobackup/projects/icesat2/ATL14_processing/rel005/south/AA/input_args_AA.txt --min_xy 360000
-setup_slurm_run.py --run_name AA"_matched_north" -q 1415_queue_AA"_matched.txt" --time 04:00:00  -e ATL14 --lines_per_task 4
+setup_ATL1415_run.py --run_name AA"_matched_north" -q 1415_queue_AA"_matched.txt" --time 04:00:00  -e ATL14 --lines_per_task 4
 
 # setup Antarctica south:
 make_ATL1415_queue.py prelim /discover/nobackup/projects/icesat2/ATL14_processing/rel005/south/AA_44km/input_args_AA_44km.txt --max_xy 440000
-setup_slurm_run.py --run_name AA"_prelim_south" -q 1415_queue_AA"_prelim.txt" --time 06:00:00  -e ATL14
+setup_ATL1415_run.py --run_name AA"_prelim_south" -q 1415_queue_AA"_prelim.txt" --time 06:00:00  -e ATL14
 
 # setup Antarctica matched south
 make_ATL1415_queue.py matched /discover/nobackup/projects/icesat2/ATL14_processing/rel005/south/AA_44km/input_args_AA_44km.txt --max_xy 440000
-setup_slurm_run.py --run_name AA"_matched_south" -q 1415_queue_AA"_matched.txt" --time 04:00:00  -e ATL14
+setup_ATL1415_run.py --run_name AA"_matched_south" -q 1415_queue_AA"_matched.txt" --time 04:00:00  -e ATL14
 
 # mosaic the northern tiles:
 make_200km_tiles.py  ~/shared/ATL14_processing/rel005/south/AA AA -t 2018.75,2026.0
